fca4ecd52679f17edc754f5ab48235f7632184d3
Oscar Umana <oscarumana1021@gmail.com>
2023-02-21 15:49:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+let theme = createTheme({
+  typography: {
+    fontFamily: ["Share Tech Mono", "monospace"].join(","),
+  },
+  palette: {
+    primary: {
+      light:"#610094",
+      main:"#3F0071",
+      dark:"#291561",
+    },
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <h1>Hello</h1>
+    </ThemeProvider>
   );
 }
 
